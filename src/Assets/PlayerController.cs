41a8a37e9e5a752b955bf0390cb39511ee6ff9dc
Roseburrow <thomas.brewer@protonmail.com>
2019-04-21 21:06:14 +0100

    Initial Player Controller

---
+++ src/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 7;
+
+    float screenHalfWidthInWorldUnits = 9.5f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float velocity = inputX * speed;
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+        if (transform.position.x < -screenHalfWidthInWorldUnits) 
+        {
+            transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
+        }
+
+		if (transform.position.x > screenHalfWidthInWorldUnits) 
+        {
+            transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+        }
+    }
+}
