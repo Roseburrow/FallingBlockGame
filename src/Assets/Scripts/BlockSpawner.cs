95f9a128567bd6e99875dbc34649b85b02b5c9b1
Roseburrow <thomas.brewer@protonmail.com>
2019-04-21 21:47:26 +0100

    BlockSpawner script and associated prefab

---
+++ src/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public GameObject fallingBlockPrefab;
+    Vector2 screenHalfSizeWorldUnits;
+
+    public float secondsBetweenSpawn = 1;
+    float nextSpawnTime;
+
+    void Start()
+    {
+        //Get screen width for spawning in valid places.
+        screenHalfSizeWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+    }
+
+    void Update()
+    {
+        if (Time.time > nextSpawnTime) {
+            nextSpawnTime = Time.time + secondsBetweenSpawn;
+
+            //Gets a random position where x is between the screen width and the y is half the screen width.
+            Vector2 spawnPosition = new Vector2(
+                Random.Range(-screenHalfSizeWorldUnits.x, screenHalfSizeWorldUnits.x),
+                screenHalfSizeWorldUnits.y + 0.5f
+            );
+            Instantiate(fallingBlockPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -11,23 +11,23 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         //Calculate the screen width in world unity by aspect ratio * orthographic size.
-        //Then add the player width / 2 to account for the player.
-        float halfPlayerWidth = transform.localScale.x / 2f;
-        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+        //Player width is accounted for in Update().
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize;
     }
 
     void Update()
-    {
+    {   
         float inputX = Input.GetAxis("Horizontal");
         float velocity = inputX * speed;
         transform.Translate(Vector2.right * velocity * Time.deltaTime);
 
-        if (transform.position.x < -screenHalfWidthInWorldUnits) 
+        float halfPlayerWidth = transform.localScale.x / 2f;
+        if (transform.position.x + halfPlayerWidth < -screenHalfWidthInWorldUnits) 
         {
             transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
         }
 
-		if (transform.position.x > screenHalfWidthInWorldUnits) 
+		if (transform.position.x - halfPlayerWidth > screenHalfWidthInWorldUnits) 
         {
             transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
         }
